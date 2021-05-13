ab9708c5f0aae0a8df575f7ba445e137873d0a2a
ranga608667 <rangarajan.murugan@cognizant.com>
2021-05-13 17:34:26 -0500

    initial commit

---
+++ src/main/java/com/example/springAssignment_TextService/SpringAssignmentTextServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springAssignment_TextService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssignmentTextServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAssignmentTextServiceApplication.class, args);
+	}
+
+}
