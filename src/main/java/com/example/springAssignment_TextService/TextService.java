44a270153a827fd584bc88553986085ca2852e93
ranga608667 <rangarajan.murugan@cognizant.com>
2021-05-13 18:01:59 -0500

    Endpoint 1 - Camelize - 1st use case completed

---
+++ src/main/java/com/example/springAssignment_TextService/TextService.java
@@ -0,0 +1,22 @@
+package com.example.springAssignment_TextService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RestController
+public class TextService {
+
+    @GetMapping("/camelize")
+    public String stringCamelCase(@RequestParam String original){
+        String[] newStrings=  original.split("_");
+        String camelCaseWord = newStrings[0];
+        for (int i = 1; i < newStrings.length; i++) {
+             camelCaseWord += (newStrings[i].substring(0,1).toUpperCase() + newStrings[i].substring(1));
+        }
+
+        return camelCaseWord;
+    }
+}
---
+++ src/test/java/com/example/springAssignment_TextService/TextServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.springAssignment_TextService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TextService.class)
+public class TextServiceTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void stringCamelize() throws Exception{
+        RequestBuilder request=get("/camelize?original=this_is_a_thing");
+        this.mvc.perform(request).andExpect(status().isOk())
+                .andExpect(content().string("thisIsAThing"));
+
+    }
+}
